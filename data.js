abbf0406fef96c5c1afc576ada0518d63943fa8b
ArtTheBeing <137590303+ArtTheBeing@users.noreply.github.com>
2023-06-30 15:39:04 -0500

    fetch initialized

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
-
+fetch('https://api.nasa.gov/') //key BHjMJOB3xoB3rqfMEAjHFuachbwBNaNV82KYux9W
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the returned data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error('Error:', error);
+  });
